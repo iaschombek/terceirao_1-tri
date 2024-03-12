95752b9eb057b85b70434af0b65d24706521db9a
Fabiana Iaschombek Costa <105210760+iaschombek@users.noreply.github.com>
2024-03-12 10:22:17 -0300

    888888

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const botoes = document.querySelectorAll(".botao")
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function{
-        for (let j = 0; j < botoes.length;){
-            botoes[j].classList.rempve("ativo");
+        for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
     }
