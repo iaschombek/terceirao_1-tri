c7c8727f38ac5e1c34e01498e59e47f0fedb4bb1
Fabiana Iaschombek Costa <105210760+iaschombek@users.noreply.github.com>
2024-04-30 10:57:23 -0300

    [[[[[[[

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-01-01T23:59:59");
+const tempoObjetivo1 = new Date("2024-12-15T23:59:59");
 const tempoObjetivo2 = new Date("2025-01-01T23:59:59");
 const tempoObjetivo3 = new Date("2025-01-01T23:59:59");
 const tempoObjetivo4 = new Date("2025-01-01T23:59:59");
